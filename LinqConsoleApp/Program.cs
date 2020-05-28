28ebb863da3c6c545377f3c0d82c2409ab650a6c
Adam <adamdox@gmail.com>
2020-05-29 00:32:26 +0200

    Add completed cw9

---
+++ LinqConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LinqConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var linqSamples = new LinqSamples();
+            linqSamples.Przyklad1();
+            linqSamples.Przyklad2();
+            linqSamples.Przyklad3();
+            linqSamples.Przyklad4();
+            linqSamples.Przyklad5();
+            linqSamples.Przyklad6();
+            linqSamples.Przyklad7();
+            linqSamples.Przyklad8();
+            linqSamples.Przyklad9();
+            linqSamples.Przyklad10();
+            linqSamples.Przyklad11();
+            linqSamples.Przyklad12();
+        }
+    }
+}
